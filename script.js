29273382eb01a36ab1044dd00090d1afd021c1c5
Gabin BROCHARD <gbrochard2023@campus-eni.fr>
2024-06-05 08:48:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+actualiserPosition()
+setInterval(actualiserPosition, 10000);
+
+let positionAnterieur = [];
+
+
+async function actualiserPosition() {
+    try {
+        const response = await fetch('http://api.open-notify.org/iss-now.json')
+        const json = await response.json()
+        console.log("json = ", json)
+
+        let position = json.iss_position
+        console.log("position = ", position)
+
+        let latitude = position.latitude
+        let longitude = position.longitude
+
+        const container = document.getElementById('position')
+        container.textContent = "Latitude = " + latitude + " | Longitude = " + longitude
+
+
+        var myIcon = L.icon({
+            iconUrl: "./img/marker-station-spacial.png",
+            iconSize: [38, 38],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],
+            shadowSize: [68, 95],
+            shadowAnchor: [22, 94]
+
+        });
+
+
+
+        var map = L.map('map').setView([latitude, longitude], 5);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+
+        var marker = L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
